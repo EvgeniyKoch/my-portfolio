2d967bd9b9510dfc63595893b8b9192851bafc17
Evgeniy Kochetov <evgkoch@list.ru>
2018-01-14 21:01:54 +0300

    map

--- src/js/main.js
+++ src/js/main.js
@@ -83,195 +83,195 @@ if(login ) {
 
 
 //Google-map
-// if(document.querySelector('#map')){
-//     function initMap() {
-//         map = new google.maps.Map(document.getElementById('map'), {
-//             center: {lat: 51.690372, lng: 39.252002},
-//             zoom: 14,
-//             styles:[
-//                 {
-//                     "featureType": "administrative.country",
-//                     "elementType": "labels.text",
-//                     "stylers": [
-//                         {
-//                             "color": "#737373"
-//                         },
-//                         {
-//                             "weight": "0.01"
-//                         }
-//                     ]
-//                 },
-//                 {
-//                     "featureType": "landscape",
-//                     "elementType": "all",
-//                     "stylers": [
-//                         {
-//                             "saturation": "97"
-//                         },
-//                         {
-//                             "color": "#ffffff"
-//                         },
-//                         {
-//                             "visibility": "simplified"
-//                         },
-//                         {
-//                             "lightness": "81"
-//                         }
-//                     ]
-//                 },
-//                 {
-//                     "featureType": "landscape.natural.landcover",
-//                     "elementType": "all",
-//                     "stylers": [
-//                         {
-//                             "saturation": "100"
-//                         },
-//                         {
-//                             "lightness": "100"
-//                         },
-//                         {
-//                             "gamma": "10.00"
-//                         }
-//                     ]
-//                 },
-//                 {
-//                     "featureType": "poi",
-//                     "elementType": "all",
-//                     "stylers": [
-//                         {
-//                             "saturation": "100"
-//                         },
-//                         {
-//                             "lightness": "100"
-//                         },
-//                         {
-//                             "gamma": "10.00"
-//                         },
-//                         {
-//                             "weight": "0.01"
-//                         }
-//                     ]
-//                 },
-//                 {
-//                     "featureType": "poi.attraction",
-//                     "elementType": "labels.text.fill",
-//                     "stylers": [
-//                         {
-//                             "color": "#565656"
-//                         }
-//                     ]
-//                 },
-//                 {
-//                     "featureType": "poi.business",
-//                     "elementType": "labels.text.fill",
-//                     "stylers": [
-//                         {
-//                             "color": "#565656"
-//                         }
-//                     ]
-//                 },
-//                 {
-//                     "featureType": "poi.government",
-//                     "elementType": "labels.text.fill",
-//                     "stylers": [
-//                         {
-//                             "color": "#565656"
-//                         }
-//                     ]
-//                 },
-//                 {
-//                     "featureType": "poi.medical",
-//                     "elementType": "labels.text.fill",
-//                     "stylers": [
-//                         {
-//                             "color": "#565656"
-//                         }
-//                     ]
-//                 },
-//                 {
-//                     "featureType": "poi.park",
-//                     "elementType": "all",
-//                     "stylers": [
-//                         {
-//                             "saturation": "100"
-//                         },
-//                         {
-//                             "lightness": "100"
-//                         },
-//                         {
-//                             "gamma": "10.00"
-//                         }
-//                     ]
-//                 },
-//                 {
-//                     "featureType": "poi.park",
-//                     "elementType": "labels.text.fill",
-//                     "stylers": [
-//                         {
-//                             "color": "#565656"
-//                         }
-//                     ]
-//                 },
-//                 {
-//                     "featureType": "poi.place_of_worship",
-//                     "elementType": "labels.text.fill",
-//                     "stylers": [
-//                         {
-//                             "color": "#565656"
-//                         }
-//                     ]
-//                 },
-//                 {
-//                     "featureType": "poi.school",
-//                     "elementType": "labels.text.fill",
-//                     "stylers": [
-//                         {
-//                             "color": "#565656"
-//                         }
-//                     ]
-//                 },
-//                 {
-//                     "featureType": "poi.sports_complex",
-//                     "elementType": "labels.text.fill",
-//                     "stylers": [
-//                         {
-//                             "color": "#565656"
-//                         }
-//                     ]
-//                 },
-//                 {
-//                     "featureType": "road",
-//                     "elementType": "all",
-//                     "stylers": [
-//                         {
-//                             "saturation": "-70"
-//                         },
-//                         {
-//                             "lightness": "43"
-//                         }
-//                     ]
-//                 },
-//                 {
-//                     "featureType": "water",
-//                     "elementType": "all",
-//                     "stylers": [
-//                         {
-//                             "color": "#39d2ca"
-//                         }
-//                     ]
-//                 }
-//             ]
-//         });
-//
-//         var marker = new google.maps.Marker({
-//             position: {lat: 51.697354, lng: 39.269249},
-//             map: map,
-//             title: 'Евгений',
-//             icon: '../images/icons/map_marker.svg'
-//         });
-//
-//     }
-// }
+if(document.querySelector('#map')){
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 51.690372, lng: 39.252002},
+            zoom: 14,
+            styles:[
+                {
+                    "featureType": "administrative.country",
+                    "elementType": "labels.text",
+                    "stylers": [
+                        {
+                            "color": "#737373"
+                        },
+                        {
+                            "weight": "0.01"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "landscape",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "saturation": "97"
+                        },
+                        {
+                            "color": "#ffffff"
+                        },
+                        {
+                            "visibility": "simplified"
+                        },
+                        {
+                            "lightness": "81"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "landscape.natural.landcover",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "saturation": "100"
+                        },
+                        {
+                            "lightness": "100"
+                        },
+                        {
+                            "gamma": "10.00"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "saturation": "100"
+                        },
+                        {
+                            "lightness": "100"
+                        },
+                        {
+                            "gamma": "10.00"
+                        },
+                        {
+                            "weight": "0.01"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi.attraction",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#565656"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi.business",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#565656"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi.government",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#565656"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi.medical",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#565656"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi.park",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "saturation": "100"
+                        },
+                        {
+                            "lightness": "100"
+                        },
+                        {
+                            "gamma": "10.00"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi.park",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#565656"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi.place_of_worship",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#565656"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi.school",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#565656"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi.sports_complex",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#565656"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "saturation": "-70"
+                        },
+                        {
+                            "lightness": "43"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "color": "#39d2ca"
+                        }
+                    ]
+                }
+            ]
+        });
+
+        var marker = new google.maps.Marker({
+            position: {lat: 51.697354, lng: 39.269249},
+            map: map,
+            title: 'Евгений',
+            icon: 'evgeniykoch.github.io/my-portfolio.io/build/images/icons/map_marker.svg'
+        });
+
+    }
+}
 
 
 
@@ -321,7 +321,7 @@ $(document).ready(function(){
 
 
 //Slider
-if(document.querySelectorAll('#slides .slide')){
+if(document.querySelectorAll('.works__slider')){
     (function () {
         const
             slider = {
